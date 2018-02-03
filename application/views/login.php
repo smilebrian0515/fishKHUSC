eaffda3ccb6a6bd390d5bf7428df2b6d0b3635ba
謹誠 <smilebrian0515@gmail.com>
2018-02-03 11:00:56 +0800

    簡易登入頁面

---
+++ application/views/login.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+</head>
+<body>
+
+	<form align="center">
+		<input type="text" placeholder="電子郵件地址"><br>
+		<input type="text" placeholder="確認電子郵件地址"><br>
+		<input type="password" placeholder="密碼"><br>
+		<input type="password" placeholder="確認密碼"><br>
+		<input type="submit" value="送出">
+		<input type="reset" value="清空">
+	</form>
+
+</body>
+</html>
