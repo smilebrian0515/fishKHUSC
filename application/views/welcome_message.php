c155321dcd273c521a6d6b2c99f0c1cf002392eb
hsu <circus0983@gmail.com>
2018-02-03 17:22:04 +0800

    首頁介面合併

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,14 +1,21 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-
-</head>
-<body>
-	<a href="./index.php/register/">註冊</a>
-	<a href="./index.php/login/">登入</a>
-</body>
+	<html>
+		<head>
+			<meta charset="utf-8">
+			<title></title>
+			<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+			<style>
+				body{ background-color: lightblue;}
+				div{font-size:20px}
+			</style>
+		</head>
+	<body>
+		<h1 align="center">歡迎使用永續經營&認證技術協助系統</h1>
+		<div align="center">
+			<a href="./index.php/register/" class="w3-button w3-block w3-blue w3-section w3-padding">註冊</a>
+			<a href="./index.php/login/" class="w3-button w3-block w3-blue w3-section w3-padding">登入</a>
+		</div>
+	</body>
 </html>
